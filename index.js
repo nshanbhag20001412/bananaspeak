6bbae00536659adc910bd5f1d811cbc1dba18e49
nshanbhag20001412 <nshanbhag1412@gmail.com>
2021-07-31 11:22:17 +0530

    deleted unnecessary content

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var outputDiv = document.querySelector("#output");
 
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/groot.json"
 
 
 function getTranslationURL(input) {
